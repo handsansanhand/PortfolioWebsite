c874e06f02effda12ecf697a6cfdd332efc3c240
handsansanhand <102315833+handsansanhand@users.noreply.github.com>
2025-05-29 23:57:05 +0100

    about page implemented

--- portfolio/src/components/Aboutpage.jsx
+++ portfolio/src/components/Aboutpage.jsx
@@ -1,8 +1,29 @@
+import './Aboutpage.css'
+import Image from 'react-bootstrap/Image';
 
+/*
+1) About me section
 
+2) Education and Skills
+*/
 function Aboutpage() {
     return (
-      <h1>HIIIII</h1>
+        <>
+        <div className='aboutMeContainer'> 
+          <div className="aboutMeSection">
+                <div className="aboutMeTitle">About Me</div>
+                <div className="aboutMeText">
+                    <p>Hi, I’m Jack Wright, a recent Computer Science graduate passionate about building all things software. I enjoy working on both front-end and back-end technologies and love learning new tools and frameworks!</p>
+                    <p>I’m currently seeking opportunities where I can contribute my skills, grow as a developer, and help create impactful software solutions. Feel free to get in touch - I’d love to connect!</p>
+                </div>
+            </div>  
+           <div className="rightSection">
+                <div className="rightSectionTitle">Skills</div>
+            </div>  
+        </div>
+        
+        </>
+      
     ); 
 }
 
