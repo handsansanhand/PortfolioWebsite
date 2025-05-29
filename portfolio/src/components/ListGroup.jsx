9ffba3d1a594cd8a9c72768be5ff8ad990383d20
handsansanhand <102315833+handsansanhand@users.noreply.github.com>
2025-05-29 18:37:23 +0100

    project set up

---
+++ portfolio/src/components/ListGroup.jsx
@@ -0,0 +1,16 @@
+import { Fragment } from "react";
+
+function ListGroup() {
+    return (
+        <Fragment>
+            <h1>List</h1>
+            <ul className="list-group">
+                <li className="list-group-item">list 1</li>
+            </ul>
+
+        </Fragment>
+
+    );
+
+}
+export default ListGroup;
