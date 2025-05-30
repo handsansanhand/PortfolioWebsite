742cede7c4fddbfd58e53338e29b6766f4517753
handsansanhand <102315833+handsansanhand@users.noreply.github.com>
2025-05-30 18:53:43 +0100

    technologies used added

--- portfolio/src/components/Projectdetails.jsx
+++ portfolio/src/components/Projectdetails.jsx
@@ -1,3 +1,4 @@
+import LogoItem from './Logoitem';
 import './Projectdetails.css';
 
 function Projectdetails({ selectedProject }) {
@@ -7,11 +8,38 @@ function Projectdetails({ selectedProject }) {
     'Details for Excelsior LLM...',
     'Details for Cascadia...'
   ];
+  const logoItems = [
+      [
+    { src: "Java_programming_language_logo.svg.png", alt: "Java Logo", label: "Java" },
+    { src: "springboot.png", alt: "Spring Boot Logo", label: "Spring Boot" },
+    { src: "docker.png", alt: "Docker Logo", label: "Docker" },
+    { src: "react.png", alt: "React Logo", label: "React" },
+    { src: "postgresql.png", alt: "GitHub Logo", label:"PostgreSQL" }, 
+    { src: "github.png", alt: "GitHub Logo", label:"GitHub"}
+  ],
+  [
+    { src: "Java_programming_language_logo.svg.png", alt: "Java Logo", label: "Java" },
+    { src: "springboot.png", alt: "SpringBoot Logo", label: "SpringBoot" },
+    { src: "docker.png", alt: "Docker Logo", label: "Docker" },
+    { src: "github.png", alt: "GitHub Logo", label: "GitHub" },
+    { src: "maven.png", alt: "Maven Logo", label: "Maven" },
+  ],
+  [
+    { src: "Java_programming_language_logo.svg.png", alt: "Java Logo", label: "Java" },
+    { src: "github.png", alt: "GitHub Logo", label: "GitHub" },
+    { src: "Python-logo-notext.svg.png", alt: "Python Logo", label: "Python" },
+  ],
+  [
+    { src: "Java_programming_language_logo.svg.png", alt: "Java Logo", label: "Java" },
+    { src: "github.png", alt: "GitHub Logo", label: "GitHub" },
+    { src: "maven.png", alt: "Maven Logo", label: "Maven" },
+  ],
+  ];
    const gitHubLink = [
     'https://github.com/handsansanhand/ToDoList',
-    'Details for HVAC Based Building Simulator...',
-    'Details for Excelsior LLM...',
-    'Details for Cascadia...'
+    'https://github.com/handsansanhand/Final-Year-Project---21467574',
+    'https://github.com/jurmangandar/software_project_3',
+    'https://github.com/handsansanhand/Cascadia---Group-18'
   ];
 
   return (
@@ -22,14 +50,32 @@ function Projectdetails({ selectedProject }) {
                
   <div className="bottomContainer">
     <div className="bottomColumn">
-      <h3>Column 1</h3>
-      <p>Details for the first thing.</p>
+      <div className="containerTitle">Methodologies & Tools</div>
+      <ul>
+        <li>Microservices</li>
+        <li>RESTful APIs</li>
+        <li>Persistent Data Handling</li>
+        <li>SQL</li>
+    </ul>
     </div>
     <div className="technologiesUsed">
-      <div className="technologiesUsedTitle">Technologies Used</div>
-      <p>Details for the second thing.</p>
+      <div className="containerTitle">Technologies Used</div>
+       <div className="technologiesUsedLogos">
+    {logoItems[selectedProject].map((item, index) => (
+      <LogoItem key={index} src={item.src} alt={item.alt} label={item.label} />
+    ))}
+  </div>
     </div>
-  </div>     
+  </div> <div className="gitHubLinkContainer">
+                GitHub Link:
+  <a 
+    href={gitHubLink[selectedProject]} 
+    target="_blank" 
+    rel="noopener noreferrer"
+  >
+    {gitHubLink[selectedProject]}
+  </a>
+        </div>    
             </div>
       ) : (
         <div>Select a project to see details</div>
