fd5e5200b583e041e56483d6e05a4bc48e1f5c26
handsansanhand <102315833+handsansanhand@users.noreply.github.com>
2025-06-02 20:51:30 +0100

    push

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -4,6 +4,8 @@ import Button from 'react-bootstrap/Button';
 import { useNavigate, useLocation } from 'react-router-dom';
 import Image from 'react-bootstrap/Image';
 import { motion } from 'framer-motion';
+import picOfMe from '../assets/picOfMe.jpg';
+
 import './Homepage.css'
 function Homepage() {
       const navigate = useNavigate();
@@ -37,7 +39,7 @@ function Homepage() {
                          animate={{ opacity: 1, y: 0 }}
                         transition={{ duration: 0.6, delay: 0.9 }}
                         >
-                            <Image src="/PortfolioWebsite/assets/picOfMe.jpg" roundedCircle className="roundedImage"/> 
+                            <Image src={picOfMe} roundedCircle className="roundedImage"/> 
                         </motion.div>
                             <motion.div
                          initial={{ opacity: 0, y: 30 }}
